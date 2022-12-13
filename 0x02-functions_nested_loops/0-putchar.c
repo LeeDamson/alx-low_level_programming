29f0ee3eabc90e4820ede9f8c21d9ff72358999f
LeeDamson <damsoncolin@gmail.com>
2022-12-13 14:48:14 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,39 +1,30 @@
 #include "main.h"
 
-        /**
+/**
+ * main - print _putchar
+ * is written by me
+ * Return: 0
+ */
 
-         * main - print _putchar
+int main(void)
+{
+	_putchar('_');
 
-         * is written by mbah
+	_putchar('p');
 
-         * Return: 0
+	_putchar('u');
 
-         */
+	_putchar('t');
 
+	_putchar('c');
 
+	_putchar('h');
 
-        int main(void)
+	_putchar('a');
 
-        {
+	_putchar('r');
 
-        _putchar('_');
+	_putchar('\n');
 
-        _putchar('p');
-
-        _putchar('u');
-
-        _putchar('t');
-
-        _putchar('c');
-
-        _putchar('h');
-
-        _putchar('a');
-
-        _putchar('r');
-
-        _putchar('\n');
-
-        return (0);
-
-        }
+	return (0);
+}
